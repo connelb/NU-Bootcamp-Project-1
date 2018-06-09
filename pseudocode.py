2a17c372e657cecd1f262da1657c0db784368d27
septober1 <septober1@gmail.com>
2018-06-09 09:59:51 -0500

    began pseudocode for handling geopandas df's

--- pseudocode.py
+++ pseudocode.py
@@ -5,7 +5,7 @@
 #   Total Medicare Discharges by State
 #   Most common DRG by State
 #   Total Medicare Payments by State
-#   Difference between Medicare Coverage and Average Covered Charges by State
+#   Difference between Medicare Payments and Average Covered Charges by State
 #   Total Medicare Discharges over Time
 #   Most common DRG over Time (by State?)
 #   Total Medicare Payments Over Time (by State?)
